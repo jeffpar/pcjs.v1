0e11255988db0a16599f7a06b4bc0a11479969f9
Jeff Parsons <Jeff@pcjs.org>
2016-04-14 14:24:52 -0700

    Support for "private" builds

---
+++ modules/shared/lib/private.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview Definitions for PRIVATE configurations.
+ * @author <a href="mailto:Jeff@pcjs.org">Jeff Parsons</a>
+ * @version 1.0
+ * Created 2016-Apr-14
+ *
+ * Copyright © 2012-2016 Jeff Parsons <Jeff@pcjs.org>
+ *
+ * This file is part of PCjs, which is part of the JavaScript Machines Project (aka JSMachines)
+ * at <http://jsmachines.net/> and <http://pcjs.org/>.
+ *
+ * PCjs is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * PCjs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with PCjs.  If not,
+ * see <http://www.gnu.org/licenses/gpl.html>.
+ *
+ * You are required to include the above copyright notice in every source code file of every
+ * copy or modified version of this work, and to display that copyright notice on every screen
+ * that loads or runs any version of this software (see Computer.COPYRIGHT).
+ *
+ * Some PCjs files also attempt to load external resource files, such as character-image files,
+ * ROM files, and disk image files. Those external resource files are not considered part of the
+ * PCjs program for purposes of the GNU General Public License, and the author does not claim
+ * any copyright as to their contents.
+ */
+
+"use strict";
+
+/* global PRIVATE: true */
+
+/*
+ * PRIVATE enables certain private features that we don't want enabled on the public web server, so by default,
+ * PRIVATE is false.  For Jekyll configurations, if site.pcjs.private is set, this file gets included, overriding
+ * the default.
+ */
+PRIVATE = true;
