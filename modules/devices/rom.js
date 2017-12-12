4087fcb29ce6197bff4f6e9224cf5f780e87a965
Jeff Parsons <Jeff@pcjs.org>
2017-12-12 15:26:19 -0800

    Fixed a binding issue with the compiled versions of all the calculator and LED devices

--- modules/devices/rom.js
+++ modules/devices/rom.js
@@ -108,21 +108,21 @@ class ROM extends Device {
             this.cols = Math.pow(2, Math.ceil(addrLines));
             this.rows = Math.pow(2, Math.floor(addrLines));
             let configLEDs = {
-                class:           "LED",
-                bindings:        {container: config.bindings[ROM.BINDING.ARRAY]},
-                type:            LED.TYPE.ROUND,
-                cols:            this.cols,
-                rows:            this.rows,
-                color:           config['colorROM'] || "green",
-                backgroundColor: config['backgroundColorROM'] || "black",
-                persistent:      true
+                "class":            "LED",
+                "bindings":         {"container": this.getBindingID(ROM.BINDING.ARRAY)},
+                "type":             LED.TYPE.ROUND,
+                "cols":             this.cols,
+                "rows":             this.rows,
+                "color":            config['colorROM'] || "green",
+                "backgroundColor":  config['backgroundColorROM'] || "black",
+                "persistent":       true
             };
             this.ledArray = new LED(idMachine, idDevice + "LEDs", configLEDs);
             this.clearArray();
             let configInput = {
                 "class":        "Input",
                 "location":     [0, 0, this.ledArray.widthView, this.ledArray.heightView, this.cols, this.rows],
-                "bindings":     {"surface": config.bindings[ROM.BINDING.ARRAY]}
+                "bindings":     {"surface": this.getBindingID(ROM.BINDING.ARRAY)}
             };
             this.ledInput = new Input(idMachine, idDevice + "Input", configInput);
             this.sCellDesc = this.getBindingText(ROM.BINDING.CELLDESC);
