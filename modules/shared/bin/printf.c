1cf00ff75f15f45667825b3def1fee37e88f5b6f
Jeff Parsons <Jeff@pcjs.org>
2019-07-24 18:50:23 -0700

    Added work-around for fullscreen changes in browsers (eg, newer versions of Chrome)

--- modules/shared/bin/printf.c
+++ modules/shared/bin/printf.c
@@ -6,5 +6,7 @@ int main()
     printf("hello %5s\n", "world");
     printf("%#01x %#02x %#03x %#04x\n", n, n, n, n);
     printf("%#07x 0x%07x %#7x 0x%7x\n", n, n, n, n);
+    float f = 3.14159;
+    printf("%8.2f %.3f\n", f, f);
     return 0;
 }
